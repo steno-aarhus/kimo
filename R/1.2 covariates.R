51917bf7e6148419b8f2447d263541a08a4a1e49
Jie_evezhang <arbor.jiezhang@gmail.com>
2023-10-20 15:10:37 +0000

    script for smoking

---
+++ R/1.2 covariates.R
@@ -0,0 +1,10 @@
+##################################################
+##create covariates###############################
+##################################################
+#smoking-FieldID:20116: never, current, former
+#<https://biobank.ndph.ox.ac.uk/ukb/field.cgi?id=20116>
+#create combined variable of smoking derived from smoking status and dosage combining p20116 and p3456 (number of cigarettes currently
+#<https://biobank.ndph.ox.ac.uk/ukb/field.cgi?id=3456>).
+#recoding p3456, -10 to 0. -1, -3 to missing
+
+
