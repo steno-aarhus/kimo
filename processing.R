834ade22a2e306fc3a7e724de463a4ae893ce2f3
Jie_evezhang <arbor.jiezhang@gmail.com>
2023-11-23 22:09:13 +0000

    add code

---
+++ processing.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+# Processing
+
+# Download saved data
+source(here::here("R/data_start.R"))
+
+# Run all data management
+# source(here::here("R/data_management.R"))
+# source diet_data
+# source outcome variables
+
+# Save the changes as parquet and upload to the RAP folder for easy download
+# next time you sign in
+# arrow::write_parquet(data_csv, here("data/data.parquet"))
+
+# Upload to the project RAP folder.
+ukbAid::upload_data(here("data/data.parquet"), username = "jiezhang")
+
